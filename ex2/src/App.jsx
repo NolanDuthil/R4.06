07e407609d5b064f75917086c28b7db2b4b5cb2c
Nolan <144225548+NolanDuthil@users.noreply.github.com>
2024-02-13 11:58:29 +0100

    debut ex2

---
+++ ex2/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './css/base.css';
+
+function App() {
+
+  const data = {
+    title: "Prix et option à l'achat",
+    desc: "Découvrez l'avenir de la mobilité avec des prix compétitifs et des options d'achat flexibles chez Auto Innovation.",
+    
+  }
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ ex2/src/component/Card/Card.jsx
@@ -0,0 +1,52 @@
+export function Card() {
+    return (
+        <div>
+            <li class="offer-box__header">
+                <h3 class="offer-box__name">Starter Package</h3>
+                <p class="offer-box__price">29 999.99</p>
+            </li>
+            <li class="offer-box__advantages">
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">✓</span>
+                <p class="advantage__text">Assurance auto complémentaire</p>
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">✓</span>
+                <p class="advantage__text">Première recharge offerte</p>
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">✓</span>
+                <p class="advantage__text">Assurance contre le vol</p>
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">✓</span>
+                <p class="advantage__text">Service de rappel d'entretien</p>
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">  &nbsp;</span>
+                <p class="advantage__text">Nettoyage hebdomadaire</p>
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">  &nbsp;</span>
+                <p class="advantage__text">Service de diagnostic à distance</p>
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">&nbsp;&nbsp;&nbsp; </span>
+                <p class="advantage__text">Service de remplissage du réservoir</p>
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">  &nbsp;</span>
+                <p class="advantage__text">Service de réparation à domicile</p>
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">  &nbsp;</span>
+                <p class="advantage__text">Entretien régulier</p>
+            </li>
+            <li class="advantage">
+                <span class="advantage__checkmark">  &nbsp;</span>
+                <p class="advantage__text">Recharge d'hydrogène illimité</p>
+            </li>
+        </div>
+    )
+}
